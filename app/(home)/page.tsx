0028baff775eb9d2241a2240440817eff08f52e7
nohsoobin <nohsoobin18@naver.com>
2024-04-15 03:17:11 +0900

    0415

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,10 @@
 "use client";
-import React, { useEffect, useRef, useCallback } from "react";
+import React, { useEffect, useRef } from "react";
 
 import CategoryList from "@/components/CategoryList";
 import { GridLayout, RoomItem } from "@/components/RoomList";
 import { useInfiniteQuery } from "react-query";
+import { useRouter } from "next/navigation";
 
 import axios from "axios";
 
@@ -11,67 +12,76 @@ import { RoomType } from "@/interface";
 import { Loader, LoaderGrid } from "@/components/Loader";
 import useIntersectionObserver from "@/hooks/useIntersectionObserver";
 
-export default function Home() {
-  const ref = useRef<HTMLDivElement | null>(null);
-  const pageRef = useIntersectionObserver(ref, {});
-  const isPageEnd = !!pageRef?.isIntersecting;
+import { BsMap } from "react-icons/bs";
+import { MapButton } from "@/components/Map";
 
-  const fetchRooms = async ({ pageParam = 1 }) => {
-    const { data } = await axios("/api/rooms?page=" + pageParam, {
-      params: {
-        limit: 12,
-        page: pageParam,
-      },
+export default function Home() {
+  try {
+    const router = useRouter();
+    const ref = useRef<HTMLDivElement | null>(null);
+    const pageRef = useIntersectionObserver(ref, {});
+    const isPageEnd = !!pageRef?.isIntersecting;
+
+    const fetchRooms = async ({ pageParam = 1 }) => {
+      const { data } = await axios("/api/rooms?page=" + pageParam, {
+        params: {
+          limit: 12,
+          page: pageParam,
+        },
+      });
+
+      return data;
+    };
+
+    const {
+      data: rooms,
+      isFetching,
+      fetchNextPage,
+      isFetchingNextPage,
+      hasNextPage,
+      isError,
+      isLoading,
+    } = useInfiniteQuery("rooms", fetchRooms, {
+      getNextPageParam: (lastPage, pages) =>
+        lastPage?.data?.length > 0 ? lastPage.page + 1 : undefined,
     });
 
-    return data;
-  };
-
-  const {
-    data: rooms,
-    isFetching,
-    fetchNextPage,
-    isFetchingNextPage,
-    hasNextPage,
-    isError,
-    isLoading,
-  } = useInfiniteQuery("rooms", fetchRooms, {
-    getNextPageParam: (lastPage, pages) =>
-      lastPage?.data?.length > 0 ? lastPage.page + 1 : undefined,
-  });
-
-  if (isError) {
-    throw new Error("Room API Fetching Error");
-  }
-
-  useEffect(() => {
-    let timerId: NodeJS.Timeout | undefined;
-
-    if (isPageEnd && hasNextPage) {
-      timerId = setTimeout(() => {
-        fetchNextPage();
-      }, 500);
+    if (isError) {
+      throw new Error("Room API Fetching Error");
     }
-  }, [fetchNextPage, hasNextPage, isPageEnd]);
 
-  return (
-    <>
-      <CategoryList />
-      <GridLayout>
-        {isLoading || isFetching ? (
-          <LoaderGrid />
-        ) : (
-          rooms?.pages?.map((page, index) => (
-            <React.Fragment key={index}>
-              {page?.data?.map((room: RoomType) => (
-                <RoomItem room={room} key={room.id} />
-              ))}
-            </React.Fragment>
-          ))
-        )}
-      </GridLayout>
-      {(isFetching || hasNextPage || isFetchingNextPage) && <Loader />}
-      <div className='w-full touch-none h-10 mb-10' ref={ref} />
-    </>
-  );
+    useEffect(() => {
+      let timerId: NodeJS.Timeout | undefined;
+
+      if (isPageEnd && hasNextPage) {
+        timerId = setTimeout(() => {
+          fetchNextPage();
+        }, 500);
+      }
+    }, [fetchNextPage, hasNextPage, isPageEnd]);
+
+    return (
+      <>
+        <CategoryList />
+        <GridLayout>
+          {isLoading || isFetching ? (
+            <LoaderGrid />
+          ) : (
+            rooms?.pages?.map((page, index) => (
+              <React.Fragment key={index}>
+                {page?.data?.map((room: RoomType) => (
+                  <RoomItem room={room} key={room.id} />
+                ))}
+              </React.Fragment>
+            ))
+          )}
+        </GridLayout>
+        <MapButton onClick={() => router.push("/map")} />
+        {(isFetching || hasNextPage || isFetchingNextPage) && <Loader />}
+        <div className='w-full touch-none h-10 mb-10' ref={ref} />
+      </>
+    );
+  } catch (e) {
+    console.error(e);
+  }
 }
