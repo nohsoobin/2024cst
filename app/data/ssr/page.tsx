8f1f640fc2593e92aa761ac8e592042edb838c26
nohsoobin <nohsoobin18@naver.com>
2024-04-15 03:54:28 +0900

    0415

--- app/data/ssr/page.tsx
+++ app/data/ssr/page.tsx
@@ -1,19 +1,23 @@
 async function getData() {
-  const res = await fetch(
-    "https://www.random.org/integers/?num=1&min=1&max=100&col=1&base=10&format=plain",
-    {
-      cache: "no-store",
+  try {
+    const res = await fetch(
+      "https://www.random.org/integers/?num=1&min=1&max=100&col=1&base=10&format=plain",
+      {
+        cache: "no-store",
+      }
+    );
+    // The return value is *not* serialized
+    // You can return Date, Map, Set, etc.
+
+    if (!res.ok) {
+      // This will activate the closest `error.js` Error Boundary
+      throw new Error("Failed to fetch data");
     }
-  );
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
 
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
+    return res.json();
+  } catch (e) {
+    console.error(e);
   }
-
-  return res.json();
 }
 
 export default async function Page() {
